dd49530e24472aaf05c7d43a284f0e0d96c36547
zhangcheng <774084394@qq.com>
2018-11-13 14:52:03 +0800

    first commit

---
+++ src/main/java/com/example/springbootstage/SpringbootStageApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStageApplication.class, args);
+    }
+}
